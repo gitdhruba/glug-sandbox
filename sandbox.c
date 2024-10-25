28eea229df1635b00bffc3a035e731133754655b
gitdhruba <sinhadhruba2001@gmail.com>
2024-10-26 01:44:54 +0530

    [ADD] monitor process for continuously monitoring the sandbox

--- sandbox.c
+++ sandbox.c
@@ -1,3 +1,8 @@
+/***********************************************************************
+     Copyright (c) 2024 GNU/Linux Users' Group (NIT Durgapur)
+     Author: Dhruba Sinha
+************************************************************************/
+
 #include <stdio.h>
 #include <sys/ptrace.h>
 #include <sys/types.h>
@@ -6,11 +11,13 @@
 #include <unistd.h>
 #include <errno.h>
 #include <stdlib.h>
-#include <signal.h>
 #include <sys/time.h>
 #include <sys/resource.h>
+
 #include "./task.h"
 #include "./syscalls.h"
+#include "./signals.h"
+
 
 #define ERR_SETSTDIN 11
 #define ERR_SETSTDOUT 12
@@ -51,12 +58,101 @@ void sandbox(const Task *task) {
 
     /********** execute task ***********/
     if (execv(task->execPath, task->args) < 0) exit(ERR_EXEC);
+
 }
 
 
+unsigned long getMemoryUsage(pid_t pid) {
+    char filename[128];
+    sprintf(filename, "/proc/%d/statm", pid);
 
-void monitor(pid_t sandboxPid, TaskResult *result) {
+    int pageSize = getpagesize() / 1024;
 
+    FILE *memUsageFile = fopen(filename, "r");
+    if (memUsageFile == NULL) return 0;
+
+    // first value is total VM size (in no of pages)
+    unsigned long memUsage = 0;
+    fscanf(memUsageFile, "%lu", &memUsage);
+    memUsage *= pageSize;
+
+    return memUsage;
+}
+
+
+void monitor(const Task *task, pid_t sandboxPid, TaskResult *result) {
+
+    int status;
+    struct rusage resourceUsage;
+    unsigned long currMemUsage = 0;
+
+    // let the first exec call execute to start the user task
+    wait4(sandboxPid, &status, 0, NULL);
+    if (WIFEXITED(status)) {
+        result->exitCode = WEXITSTATUS(status);
+        result->errorMsg = "couldn't execute program";
+        return;
+    }
+
+
+    // keep monitoring
+    int isMLE = 0;
+    int disallowedSyscallUsed = -1;
+    while (1) {   
+        // let the sandbox continue till next syscall
+        ptrace(PTRACE_SYSCALL, sandboxPid, NULL, NULL); 
+
+        // update memory usage
+        currMemUsage = getMemoryUsage(sandboxPid);
+        // check for MLE
+        if (currMemUsage > task->maxMemory) {
+            isMLE = 1;
+            kill(sandboxPid, SIGTERM);
+        }
+
+        // wait for sandbox for a state change        
+        wait4(sandboxPid, &status, 0, &resourceUsage);
+        
+        // check if exited
+        if (WIFEXITED(status)) {
+            result->status = 1;
+            result->exitCode = WEXITSTATUS(status);
+            result->execTime = ((resourceUsage.ru_utime.tv_sec + resourceUsage.ru_stime.tv_sec) * 1000) + ((resourceUsage.ru_utime.tv_usec + resourceUsage.ru_stime.tv_usec) / 1000);
+            result->memoryUsed = currMemUsage;
+            if (result->exitCode) result->errorMsg = "NZEC";
+            break;
+        }
+
+        // check if signaled
+        if (WIFSIGNALED(status)) {
+            int signal = WTERMSIG(status);
+            result->signal = signal;
+            result->execTime = ((resourceUsage.ru_utime.tv_sec + resourceUsage.ru_stime.tv_sec) * 1000) + ((resourceUsage.ru_utime.tv_usec + resourceUsage.ru_stime.tv_usec) / 1000);
+            result->memoryUsed = currMemUsage;
+
+            // if terminated due to illegal syscall
+            if ((disallowedSyscallUsed != -1) && (signal == SIGKILL)) sprintf(result->errorMsg, "illegal syscall used: %s", disallowed_syscalls[disallowedSyscallUsed]);
+            // if terminated due to TLE
+            else if ((signal == SIGXCPU) || (signal == SIGKILL)) result->errorMsg = "TLE";
+            // if terminated due to MLE
+            else if (isMLE && (signal == SIGTERM)) result->errorMsg = "MLE";
+            // if terminated due to File size limit
+            else if (signal == SIGXFSZ) result->errorMsg = "OLE";
+            // other signals
+            else sprintf(result->errorMsg, "program terminated with signal %s", signal_name[signal]);
+            
+            break;
+        }
+
+        // check for disallowed syscalls
+        long offsetForOrigRax = sizeof(long) * ORIG_RAX;
+        long syscallNumber = ptrace(PTRACE_PEEKUSER, sandboxPid, offsetForOrigRax, NULL);
+        disallowedSyscallUsed = isDisallowedSyscall(syscallNumber);
+        if (disallowedSyscallUsed != -1) kill(sandboxPid, SIGKILL);
+        
+    }
+
+    return;
 }
 
 
@@ -65,6 +161,10 @@ TaskResult secureExecute(const Task *task) {
 
     TaskResult result;
     result.status = 0;
+    result.exitCode = -1;
+    result.signal = -1;
+    result.execTime = result.memoryUsed = 0;
+    result.errorMsg = NULL;
 
     // create child process for executing task
     pid_t pid = fork();
@@ -75,7 +175,7 @@ TaskResult secureExecute(const Task *task) {
         sandbox(task);
     }
     else {                          // parent
-        monitor(pid, &result);
+        monitor(task, pid, &result);
 
     }    
 
--- syscalls.h
+++ syscalls.h
@@ -1,15 +1,36 @@
+/***********************************************************************
+     Copyright (c) 2024 GNU/Linux Users' Group (NIT Durgapur)
+     Author: Dhruba Sinha
+************************************************************************/
+
+
 #include <sys/syscall.h>
 
 
 struct syscall_entry {
-    int syscall_no;
-    char *syscall_name;
+    long syscallNo;
+    char *syscallName;
 };
 
 typedef struct syscall_entry SyscallEntry;
 
 
-// list of syscalls that are not allowed
-const SyscallEntry restricted_syscalls[] = {
+// array of syscalls that are not allowed
+const SyscallEntry disallowed_syscalls[] = {
 
 };
+
+
+/*
+    check wheather a syscall is allowed or not
+    if allowed, return -1
+    else return the index of that syscall in the array disallowed_syscalls
+*/
+int isDisallowedSyscall(long syscallNo) {
+    int n = sizeof(disallowed_syscalls) / sizeof(SyscallEntry);
+    for (int i = 0; i < n; i++) {
+        if (disallowed_syscalls[i].syscallNo == syscallNo) return i;
+    }
+
+    return -1;
+}
